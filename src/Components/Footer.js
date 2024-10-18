5815f43eec2048781a7937cd7a7a32f8ea63ab0f
Arijit Ghosh <122629070+Arijit-017@users.noreply.github.com>
2024-10-18 15:45:55 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,20 +20,20 @@ function contactus(){
         <div className="sb_footer-links">
           <div className="sb_footer-links_div">
             <h4>ISTE HIT SC</h4>
-            <a onClick="website">
+            <a onClick={website}>
               <p>The ISTE Student Chapter at Haldia Institute of Technology is your gateway to innovation in educational technology. Our platform encourages engagement in research, workshops, and collaborative projects that advance technology and education.</p>
             </a>
           </div>
 
           <div className="sb_footer-links_div">
             <h4>Useful Links</h4>
-            <a onClick="join">
+            <a onClick={join}>
               <p>Join</p>
             </a>
             <a >
               <p>About</p>
             </a>
-            <a onClick="contactus">
+            <a onClick={contactus}>
               <p>Contact Us</p>
             </a>
           </div>
